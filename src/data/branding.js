24e314cbba6da61a69da918488c97f73a30ee39a
Primus023008 <vasuanush@gmail.com>
2021-10-23 03:41:36 +0530

    branding data

--- src/data/branding.js
+++ src/data/branding.js
@@ -1,10 +1,30 @@
+import wizz from '../assets/wizz.jpg'
+import sass from '../assets/sasskart.jpg'
+import pm from '../assets/pm.jpg'
+import cfour from '../assets/c4.jpg'
+import ted from '../assets/ted.jpg'
+
 const branding = [
     {
         title:'Sasskart- Logo Design',
-        content:'Logo design for an upcoming youth centered fashion brand',
-        url:'',
-        img:'',
+        img:sass,
     },
+    {
+        title:'Wizzkids Logo',
+        img:wizz,
+    },
+    {
+        title:'Placement Maestro Campaign Carousel',
+        img:pm,
+    },
+    {
+        title:'What is C4 Carousel',
+        img:cfour,
+    },
+    {
+        title:'Ted Circles Instagram Post',
+        img:ted,
+    }
 
 ]
 export default branding;
