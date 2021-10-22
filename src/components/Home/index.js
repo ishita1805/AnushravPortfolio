3b4f735c3a34c958831215508bb8dd4fff436b19
Ishita Kabra <ishitakabra1805@gmail.com>
2021-10-22 22:38:53 +0530

    landing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import './style.css'
 import arrow from '../../assets/next.png'
+import be from '../../assets/behance.png'
+import db from '../../assets/dribbble.png'
+import ins from '../../assets/instagram.png'
+import li from '../../assets/linkedin.png'
+
 
 const Home = () => {
     return (
         <div className='home'>
             <div className='home-content'>
-                <h1>
-                    I’m a <span id='purple'>UX/UI</span> 
-                    <br/>
-                    & <span id='purple'>Product</span> Designer
-                </h1>
-                <p id='subtitle'>Building meaningful user experiences one product at a time</p>
-                <div className='buttons'>
-                    <button id='cta'>My Work</button>
-                    <button id='cta2'>Let's Chat</button>
+                <div className='home-center'>
+                    <h1>
+                        I’m a <span id='purple'>UX/UI</span> 
+                        <br/>
+                        & <span id='purple'>Product</span> Designer
+                    </h1>
+                    <p id='subtitle'>Building meaningful user experiences one product at a time</p>
+                    <div className='buttons'>
+                        <button id='cta'>My Work</button>
+                        <button id='cta2'>Let's Chat</button>
+                    </div>
+                </div>
+                <div className='icons'>
+                    <a href="https://dribbble.com/AnushravSinghal" target="__blank"><img src={db} alt="dribbble"/></a>
+                    <a href="https://www.behance.net/anushravsinghal" target="__blank"><img src={be} alt="behance"/></a>
+                    <a href="https://www.instagram.com/anushravsinghal/" target="__blank"><img src={ins} alt="instagram"/></a>
+                    <a href="https://www.linkedin.com/in/anushravsinghal/" target="__blank"><img src={li} alt="linkedin"/></a>
                 </div>
             </div>
             
