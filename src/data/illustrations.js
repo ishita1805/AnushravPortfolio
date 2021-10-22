89c04a4b2a0b5a7b4e4b3af45fbd72f5cda5f7b6
Primus023008 <vasuanush@gmail.com>
2021-10-23 03:58:57 +0530

    illustrations

--- src/data/illustrations.js
+++ src/data/illustrations.js
@@ -1,15 +1,28 @@
+import bojack from '../assets/bojack.jpg'
+import st1 from '../assets/st1.jpg'
+import st2 from '../assets/st2.jpg'
+import neymar from '../assets/neymar.jpg'
+import yoda from '../assets/yoda.png'
+import bw from '../assets/blackwidow.jpg'
+
 const illustrations = [
     {
-        title:'',
-        content:'',
-        url:'',
-        img:'',
+        img:bojack,
     },
     {
-        title:'',
-        content:'',
-        url:'',
-        img:'',
+        img:neymar,
+    },
+    {
+        img: bw,
+    },
+    {
+        img:st1,
+    },
+    {
+        img:yoda,
+    },
+    {
+        img:st2,
     }
 ]
 export default illustrations;
